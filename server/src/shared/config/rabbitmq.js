629f1dc07aaffab48a6ce4ae6bd8f7b1d22483c1
Noor <thenoorulhassan.dev@gmail.com>
2026-07-21 22:16:40 +0500

    Rabbit MQ connector

--- server/src/shared/config/rabbitmq.js
+++ server/src/shared/config/rabbitmq.js
@@ -1,26 +1,45 @@
 import config from "./index.js";
 import logger from "./logger.js";
-
-import amqplib from "amqplib";
+import amqp from "amqplib";
 
 class RabbitMqConnection {
   constructor() {
     this.channel = null;
     this.connection = null;
+    this.isConnecting = false;
   }
 
   // connect
   async connect() {
-    try {
-      if (this.connection) {
-        logger.info("RabbitMQ Already Connected");
-        return this.connection;
-      }
+    if (this.channel) {
+      return this.channel;
+    }
 
-      this.connection = await amqplib.connect(config.rabbitmq.url);
+    if (this.isConnecting) {
+      await new Promise((resolve) => {
+        const checkInterval = setInterval(() => {
+          if (!this.isConnecting) {
+            clearInterval(checkInterval);
+            resolve();
+          }
+        }, 100);
+      });
+      return this.channel;
+    }
+    try {
+      this.isConnecting = true;
+      logger.info("Connecting to RabbitMQ", config.rabbitmq.url);
+      this.connection = await amqp.connect(config.rabbitmq.url);
+      this.channel = await this.connection.createChannel();
+      await this.channel.assertQueue("HEALTH_CHECK", { durable: true });
+      this.isConnecting = false;
+      logger.info("RabbitMQ connected");
     } catch (error) {
-      logger.error("Error in RabbitMQ Connection: ", error);
+      logger.error("Error in RabbitMQ connection: ", error);
+      this.isConnecting = false;
       throw error;
     }
   }
 }
+
+export default new RabbitMqConnection();
